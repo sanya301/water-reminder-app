d67b622d76a3e84b666b85f0795f30b5865341dd
Sanya Nijhawan <sanya301@gmail.com>
2018-07-18 21:46:28 +0530

    working code after testing on chrome developer mode

---
+++ background.js
@@ -0,0 +1,99 @@
+//popup.js
+
+'use strict';
+
+var DEFAULT_SOUND = 'slow-spring-board';
+var DEFAULT_VOLUME = 1.0;
+var DEFAULT_INTERVAL = 60;
+var HOUR_MS = 1000 * 60 * 60;
+
+var audio=null
+var start_time = 10 * HOUR_MS;
+var end_time = 22 * HOUR_MS;
+
+function playSound(duckAudio) {
+
+  var currentSound = DEFAULT_SOUND;
+  audio = document.createElement('audio');
+  document.body.appendChild(audio);
+  audio.autoplay = true;
+
+  var src = currentSound + '.ogg';
+  var volume = DEFAULT_VOLUME;
+  audio.volume = volume;
+  audio.src = src;
+}
+
+function setnextAlarm() {
+  var minutes = DEFAULT_INTERVAL;
+  var d = new Date();
+
+  var now = d.getHours() * HOUR_MS +
+            d.getMinutes() * 60 * 1000 +
+            d.getSeconds() * 1000;
+  
+  var next_alarm = now + (minutes * 60 * 1000);
+  
+  if(next_alarm>end_time){
+    alert('greater than end time');
+    minutes = (start_time + (HOUR_MS*24 - now));
+    minutes = Math.ceil(minutes/(60*1000));
+  }
+  if(next_alarm<start_time){
+    alert('before start time');
+    minutes = (start_time - now);
+    minutes = Math.ceil(minutes/(60*1000));
+  }
+
+  var ss = d.getSeconds() + (minutes%1)*60;
+  var mm = d.getMinutes() + Math.floor(minutes%60);
+  var hh = d.getHours() + Math.floor(minutes/60);
+  if(parseFloat(ss)>59){
+    mm = mm + 1;
+    ss = ss%60;
+  }
+  if(parseFloat(mm)>59){
+    hh = hh + 1;
+    mm = mm%60;
+  }
+  if(parseFloat(mm) < 10){
+    mm = "0"+mm;
+  }
+  if(parseFloat(ss) < 10){
+    mm = "0"+ss;
+  }
+  var alarm_time = hh + ":" + mm + ":"+ ss;
+  chrome.storage.sync.set({'next_alarm': alarm_time});
+  chrome.alarms.create({delayInMinutes: minutes});  
+}
+
+chrome.alarms.onAlarm.addListener(function() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.notifications.create({
+      type:     'basic',
+      iconUrl:  'icon.png',
+      title:    'Time to Hydrate',
+      message:  'Everyday I\'m Guzzlin\'!',
+      buttons: [
+        {title: 'Keep it Flowing.'}
+      ],
+      priority: 0});
+  setnextAlarm()
+  playSound(true);
+});
+
+chrome.notifications.onButtonClicked.addListener(function() {
+  chrome.storage.sync.get(['minutes'], function(item) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.alarms.create({delayInMinutes: item.minutes});
+  });
+});
+
+function bginitialize() {
+  alert('bginitialize');
+  var tod = new Date();
+  chrome.storage.sync.set({'today': String(tod.getDate())});
+  chrome.alarms.create({delayInMinutes: 0.1});
+}
+
+bginitialize();
---
+++ popup.js
@@ -0,0 +1,125 @@
+//popup.js
+'use strict';
+
+var cur_wat;
+var undo_val = 0;
+
+var HOUR_MS = 1000 * 60 * 60;
+var start_time = 10 * HOUR_MS;
+var end_time = 22 * HOUR_MS;
+
+function time(ms) {
+    return new Date(ms).toISOString().slice(11, -5);
+}
+
+function check_next_alarm(){
+	var now = new Date;
+	var next_alarm = null;
+	chrome.storage.sync.get('next_alarm', function(result) {
+		next_alarm = parseFloat(result.next_alarm.slice(0,2))*HOUR_MS 
+					 + parseFloat(result.next_alarm.slice(2,4))* 60 *1000;
+		alert(next_alarm.slice(0,2));
+		var t = now.getHours() * HOUR_MS +
+    			now.getMinutes() * 60 * 1000 +
+    			now.getSeconds() * 1000;
+		if (next_alarm < t){
+			next_alarm = time(now.getTime() + 60*60*1000);
+		} 
+	});
+	return next_alarm;
+}
+
+function getWater() {
+	chrome.storage.sync.get('current_water', function (result) {
+	    cur_wat = result.current_water;
+	});
+	if(cur_wat){
+		document.getElementById('current_water').innerText = cur_wat + " mL";
+	}
+	else {
+		document.getElementById('current_water').innerText = "0 mL";
+	}
+}
+
+function updateWater(){
+	var add = document.getElementById('add_val').value;
+	var cur = document.getElementById('current_water').innerText;
+	var new_tot = parseFloat(cur) + parseFloat(add);
+	var txt = String(new_tot);
+	chrome.storage.sync.set({'current_water': txt});
+	chrome.storage.sync.set({'last_val': add});
+	document.getElementById('add_val').value = 0;
+}
+
+function undo(){
+	var cur = document.getElementById('current_water').innerText;
+	chrome.storage.sync.get('last_val', function (result) {
+	    var undo_val = result.last_val;
+	    chrome.storage.sync.set({'last_val': 0});
+	    if(parseFloat(undo_val) < 0){
+	    	return;
+	    }
+	    var old_tot = parseFloat(cur) - parseFloat(undo_val);
+		var txt = String(old_tot);
+		chrome.storage.sync.set({'current_water': txt});
+	});
+
+
+function clearAlarm() {
+  	chrome.browserAction.setBadgeText({text: ''});
+	chrome.alarms.clearAll();
+    chrome.storage.sync.set({'next_alarm': "00:00"});
+    window.close();
+    chrome.browserAction.setBadgeText({text: 'OFF'});
+	alert("All alarms for today canceled. Go to settings to restart.");
+}
+
+function updateCurrentTime() {
+	var now= new Date;  
+  	var fut = new Date();
+  	chrome.storage.sync.get('next_alarm', function(result) {
+        var d = result.next_alarm;
+        if(d=="00:00"){
+        	return;
+        }
+        var res = d.split(":")
+        fut.setHours(res[0], res[1], res[2]);
+      	
+		var left = time(fut-now);
+		if(parseFloat(left.slice(0,2))==23){
+			left = "00:00:00";
+		}
+        document.getElementById('timer').innerText = left;
+
+  	});
+}
+
+function setnewdaywater(){
+    var now = new Date;
+    chrome.storage.sync.get('today', function (result) {
+	    var d = result.today;
+	    if((parseFloat(now.getDate())-parseFloat(d))!=0){
+	    	var next_alarm = check_next_alarm();
+        	if(next_alarm==null){
+        		next_alarm = time(now.getTime() + 2*60*1000);
+        	}
+	    	chrome.storage.sync.set({'current_water': "0"});
+	    	chrome.storage.sync.set({'today': String(now.getDate())});
+	    	chrome.storage.sync.set({'last_val': "0"});
+	    	chrome.storage.sync.set({'next_alarm': next_alarm});
+	    	alert('date changed');
+	    }	
+	});
+}
+
+window.onload=setnewdaywater;
+
+updateCurrentTime();
+setInterval(updateCurrentTime, 500);
+getWater();
+setInterval(getWater, 100);
+
+document.getElementById('water_add').addEventListener('click', updateWater);
+document.getElementById('undo').addEventListener('click', undo);
+
+document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
